2fec744278ed1f0ded58ed24aea42693dd6ce749
Mihai <damiangeorge15@yahoo.com>
2022-06-17 17:23:43 +0100

    0.2

---
+++ for.py
@@ -0,0 +1,11 @@
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador +=1
+#     print(contador)
+
+for contador in range(1,1001):
+    print(contador)
+
+for i in range(0,11):
+    print(5*i)
