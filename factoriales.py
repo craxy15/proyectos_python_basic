98a1a9474b830c6737272fe4a19b28bbdd1f2cfa
Mihai <damiangeorge15@yahoo.com>
2022-08-16 20:51:17 +0100

    0.5

---
+++ factoriales.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    """
+    Calcular el factorial de n
+    n int > 0
+    return n!
+    """
+    if n == 1:
+        return 1
+    
+    return n * factorial(n - 1)
+
+n = int(input('Escribe un entero: '))
+
+print(factorial(n))
---
+++ rangos_reto.py
@@ -0,0 +1,6 @@
+#Generar numeros impares
+
+num_impar= range(1,101,2)
+
+for i in num_impar:
+    print(i)
